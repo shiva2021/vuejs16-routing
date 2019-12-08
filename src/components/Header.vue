93c631e3df0c8c431db95fc94565e09a2a428358
Shivang <shiva.chaturvedi91@gmail.com>
2019-12-08 18:33:07 +0530

    Different way of achiving an active state of toolbar button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="container">
-    <ul class="nav nav-tabs">
+    <!-- <ul class="nav nav-tabs">
       <li class="nav-item">
         <router-link to="/">Home</router-link>
       </li>
       <li class="nav-item">
         <router-link to="/user">Details</router-link>
       </li>
+    </ul> -->
+
+    <ul class="nav nav-pills">
+      
+        <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+        <router-link to="/user" tag="li" active-class="active" ><a>Details</a></router-link>
+
     </ul>
   </div>
 </template>
